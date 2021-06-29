4af6dbe0dd16b251613646f6af3795bd6c495d80
AndrewFitzpatrick <doctorfitztastic@gmail.com>
2021-06-29 11:54:47 -0400

    refactor functions

--- functions.js
+++ functions.js
@@ -9,7 +9,14 @@ var warn = document.getElementById("warning");
 var extra = document.getElementById("extra");
 
 // returns length of the input value
-var inputLength = input.value.length;
+function inputLength(){
+	return input.value.length;
+}
+
+// returns length of the list
+function listLength(){
+	return item.length;
+}
 
 // creates the warning element
 function createWarningElement() {
@@ -22,11 +29,6 @@ function createWarningElement() {
 // if new item field is empty and enter is pushed, display the warning element
 function displayWarning() {
 	warn.style.display = "block";
-}
-
-// if new item field has content and warning is shown, remove warning
-function hideWarning() {
-	if (warn.style.display === "block") {warn.style.display = "none";}
 }
 
 // check if user input is already an item on the list
@@ -46,7 +48,7 @@ function checkForDuplicates() {
 
 // display warning if duplicate is found
 function duplicateFound(arr) {
-	hideWarning();
+	hideError(warn);
 	extra.style.display = "block";
 	var yesBtn = document.createElement("button");
 	yesBtn.className = "yesBtn";
@@ -57,16 +59,16 @@ function duplicateFound(arr) {
 }
 
 
-// hide duplicate found message
-function hideDuplicateFound() {
-	if (extra.style.display === "block") {extra.style.display = "none";}
+// hide error messages
+function hideError(...params) {
+	for (let i = 0; i < params.length; i++) {
+        if (params[i].style.display === "block") {params[i].style.display = "none";}
+    }
 }
 
 // create a new list item
 function createListElement() {
-	hideWarning();
-	hideDuplicateFound();
-	
+	hideError(warn, extra);	
 	var li = document.createElement("li"); // creates an element "li"
 	var span = document.createElement("span"); // creates an element "li"
 	span.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
@@ -95,33 +97,22 @@ function createListElement() {
 	function deleteListItem(){
 		li.classList.add("delete")
 	}
-
 }
 
 // initial function call
-function addListAfterClick(){
-	if (inputLength > 0) { //makes sure that an empty input field doesn't create a li
+function addList(event){
+	if (inputLength() > 0 || inputLength() > 0 && event.which ===13) { //makes sure that an empty input field doesn't create a li
 		checkForDuplicates();
 	} else {
 		 displayWarning();
 	}
 }
 
-// initial keypress function
-function addListAfterKeypress(event) {
-	if (inputLength > 0 && event.which ===13 ) { //this now looks to see if you hit "enter"/"return"
-		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
-		checkForDuplicates();
-	} else {
-		 displayWarning();
-	}
-}
-
-enterButton.addEventListener("click",addListAfterClick);
+enterButton.addEventListener("click",addList);
 
 input.addEventListener("keypress", function (e) {
     if (e.key === 'Enter') {
-      addListAfterKeypress(e)
+      addList(e)
     }
 });
 
